6f22022b9b1ef9e4b2289a62527c01b2e973670c
denchick <denchick@yandex-team.ru>
2025-07-23 09:48:55 +0200

    minor fixes

--- debuglocal-init2shards.sql
+++ debuglocal-init2shards.sql
@@ -1,7 +1,4 @@
-create distribution ds1 column types integer;
-
-alter distribution ds1 attach relation pgbench_branches distribution key bid;
-alter distribution ds1 attach relation test1 distribution key id SCHEMA sh1;
-
+CREATE DISTRIBUTION ds1 COLUMN TYPES integer;
+ALTER DISTRIBUTION ds1 ATTACH RELATION pgbench_branches DISTRIBUTION KEY bid;
+ALTER DISTRIBUTION ds1 ATTACH RELATION test1 DISTRIBUTION KEY id SCHEMA sh1;
 CREATE KEY RANGE FROM 0 ROUTE TO sh1 FOR DISTRIBUTION ds1;
-
